4a3690f8161b02884a42308113ae73fb50585009
The-Postal-Dude <artiom.labietskii@gmail.com>
2021-11-01 23:44:25 +0300

    Add prototype of main page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
 import Greeting from "./components/Greeting";
+import MainPage from "./components/MainPage";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ReactDOM } from "react";
 import {useState} from 'react';
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" exact component={Greeting} />
           <Route path="/registration" component={Registration} />
           <Route path="/login" component={Login} />
+          <Route path="/mainpage" component={MainPage} />
         </Switch>
       </div>
     </Router>
