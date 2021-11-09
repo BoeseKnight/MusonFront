a229d02f2b21294049bf40625fe5eee59d47d407
The-Postal-Dude <artiom.labietskii@gmail.com>
2021-11-09 13:19:18 +0300

    Changed Artists and Genres pages, added like and dislike buttons.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -12,72 +12,58 @@ import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
-import { borderRadius, width } from "@mui/system";
+import { borderRadius, getContrastRatio, width } from "@mui/system";
 import SplitPane from "react-split-pane";
 import { Splitter } from "@progress/kendo-react-layout";
 import { Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
-import Pane from "./Pane";
 import Link from "@mui/material/Link";
-import GenerateListOfSongs from "./CommonComponents";
+import GenerateListOfSongs from "./ListOfSongs";
+import SidePane from "./SidePane"
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 // import Helmet from 'react-helmet';
 
 export default class MainPage extends React.Component {
   constructor(props) {
     super(props);
+    this.logoOnCLick = this.logoOnCLick.bind(this);
     this.state = {
       token: this.props.location.state.access_token,
-      buttonsList: [],
+      songs: [],
       isChart: false,
       isAll: true,
     };
-    var Song = { id: 0, song: "", artist: "", directory: "", genre: "" };
     const headers = { Authorization: this.state.token };
     this.boxRef = React.createRef();
-    var buttonStyle = {
-      margin: "10px 10px 10px 0",
-    };
     fetch("http://localhost:8080/showAllSongs", { headers })
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ buttonsList: data }, () => {
-          console.log(this.state.buttonsList);
+        this.setState({ songs: data }, () => {
+          console.log(this.state.songs);
         });
       });
   }
   componentDidMount() {
     if (this.props.active) {
-      // whatever your test might be
       this.boxRef.current.scrollIntoView();
     }
   }
   onPressImage() {
     alert("You tapped the button!");
   }
+
+  logoOnCLick() {
+    this.props.history.push("MainPage", {
+      access_token: this.state.token,
+    });
+  }
+
+  //   pause(){
+  // 	  audioEl.pause();
+  // 	}
   render() {
-    const clickSong = (song) => {
-      console.log(song.song);
-    };
-
-    const click = () => {
-      console.log("click");
-    };
-
-    const artistsOnCLick = () => {
-      this.props.history.push("Artists", {
-        access_token: this.state.token,
-      });
-    };
-
-    const genresOnCLick = () => {
-      this.props.history.push("Genres", {
-        access_token: this.state.token,
-      });
-    };
-
-    const collectionOnCLick = () => {
-      this.props.history.push("Collection", {
-        access_token: this.state.token,
-      });
+    const playAudio = () => {
+      document.getElementsByClassName("audio-element").play();
     };
 
     const chartOnClick = () => {
@@ -96,112 +82,62 @@ export default class MainPage extends React.Component {
       }
     };
 
-    const chart = () => {
-      if (this.state.isChart) {
-        return this.state.buttonsList.map(GenerateListOfSongs)}
-    };
-
     return (
       <div>
-        <div
-          style={{
-            height: "100%",
-            position: "fixed",
-			backgroundColor:"#010101",
-            width: "250px",
-            alignItems: "left",
-            display: "flex",
-            top: "0",
-            left: "0",
-          }}
-        >
-          <ul
-            style={{
-              listStyleType: "none",
-              marginTop: "0px",
-              padding: "0px",
-              textAlign: "left",
-            }}
-          >
-            <li>
-              <Link
-                style={{
-                  color: "white",
-                  fontSize: "14pt",
-                  margin: "20px",
-                  height: "2px",
-                }}
-                underline="none"
-                component="button"
-                onClick={() => {
-                  collectionOnCLick();
-                }}
-              >
-                <h4>Collection</h4>
-              </Link>
-            </li>
-            <li>
-              <Link
-                style={{
-                  color: "white",
-                  fontSize: "14pt",
-                  margin: "20px",
-                  height: "2px",
-                }}
-                underline="none"
-                component="button"
-                onClick={() => {
-                  artistsOnCLick();
-                }}
-              >
-                <h4>Artists</h4>
-              </Link>
-            </li>
-            <li>
-              <Link
-                style={{
-                  color: "white",
-                  fontSize: "14pt",
-                  margin: "20px",
-                  height: "2px",
-                }}
-                underline="none"
-                component="button"
-                onClick={() => {
-                  genresOnCLick();
-                }}
-              >
-                <h4>Genres</h4>
-              </Link>
-            </li>
-          </ul>
+        <div>
+            {SidePane(this.state.token, this.props)}
         </div>
-        <div style={{display:"flex", marginTop:"100px"}}>
+        <div style={{ display: "flex", marginTop: "100px" }}>
           <div style={{ marginLeft: "250px" }}>
             <div style={{ width: "300px", float: "left" }}>
               <img
                 onClick={this.onPressImage}
                 alt="HTML5"
-                style={{ height: "250px" , borderRadius: "10px"}}
+                style={{ height: "250px", borderRadius: "10px" }}
                 src="/images/DkG03zyV4AAFpXO.jpg"
               />
+              <div>
+                <audio id="audio-element" type="audio/mpeg"></audio>
+              </div>
+              {/* <Button
+                variant="contained"
+                size="large"
+                onClick={this.pause}
+                style={{
+                  width: "200px",
+                  backgroundColor: "#7f5af0",
+                  padding: 0,
+                }}
+              >
+                <h3>PAUSE</h3>
+              </Button> */}
+              {/* <AudioPlayer
+                autoPlay
+                src="http://localhost:8080/stream/NothingButThieves-FreeIfWeWantIt.mp3"
+                onPlay={(e) => console.log("onPlay")}
+                // other props here
+              /> */}
               <h2 style={{ margin: "0" }}>RAP</h2>
             </div>
             <div style={{ width: "300px", float: "left" }}>
               <img
                 onClick={this.onPressImage}
                 alt="HTML5"
-                style={{ height: "250px", backgroundColor: "gray" , borderRadius: "10px"}}
+                style={{
+                  height: "250px",
+                  backgroundColor: "gray",
+                  borderRadius: "10px",
+                }}
                 src="\images\linkin-park-logo.png"
               />
 
               <h2 style={{ margin: "0" }}>ROCK</h2>
             </div>
-			<div style={{ width: "300px", float: "left" }}>
+            <div style={{ width: "300px", float: "left" }}>
               <img
                 onClick={this.onPressImage}
                 alt="HTML5"
-                style={{ height: "250px", backgroundColor: "gray", borderRadius: "10px" }}
+                style={{ height: "250px", borderRadius: "10px" }}
                 src="\images\hehe.jpg"
               />
 
@@ -218,16 +154,26 @@ export default class MainPage extends React.Component {
             </div>
           </div>
         </div>
-        <div style={{display:"flex", marginLeft:"250px", justifyContent:"center", marginTop:"50px"}}>
-		<Button variant="contained" size="large" onClick={chartOnClick} style={{width:"200px", backgroundColor:"#7f5af0", padding: 0}}>
-          <h3>Chart</h3>
-        </Button>
+        <div
+          style={{
+            display: "flex",
+            marginLeft: "250px",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Button
+            variant="contained"
+            size="large"
+            onClick={chartOnClick}
+            style={{ width: "200px", backgroundColor: "#7f5af0", padding: 0 }}
+          >
+            <h3>Chart</h3>
+          </Button>
         </div>
-		<div >
-        {/* {chart()}
-              {all()} */}
-			  {chart()}
-			  </div>
+        <div>
+            {GenerateListOfSongs(this.state.songs, this.state.token)}
+        </div>
       </div>
     );
   }
